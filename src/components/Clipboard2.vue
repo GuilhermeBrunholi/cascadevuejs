800aef7e30657451ab09573b07d4ffaa0bf0ddc5
Guilherme Brunholi <gui_brunholi@hotmail.com>
2020-01-31 20:54:32 -0300

    Atualizacao 2

--- src/components/Clipboard2.vue
+++ src/components/Clipboard2.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <div id="1" class="box" style="cursor: pointer;"></div>
-      <div id="2" class="box"></div>
-      <div id="3" class="box"></div>
-      <div id="4" class="box"></div>
-      <div id="5" class="box"></div>
-      <div id="6" class="box"></div>
-      <div id="7" class="box"></div>
-      <div id="8" class="box"></div>
+      <img id="div1" class="box" />
     </div>
   </div>
 </template>
@@ -20,17 +13,27 @@ export default {
       idDelete: ""
     };
   },
+  methods: {
+    
+  },
   mounted() {
+    var reader = new FileReader();
+
+    reader.onload = function(result) {
+      let img = document.getElementById("div1");
+      img.src = result.target.result;
+      img.className = "box";
+    };
     // Recebe o valor do clipboard
-    // document.querySelector("input").onpaste = function(event) {
-    //   let items = event.clipboardData.items;
-    //   for (var itemIndex in items) {
-    //     let item = items[itemIndex];
-    //     if (item.kind == "file" && cont < 6) {
-    //       reader.readAsDataURL(item.getAsFile());
-    //     }
-    //   }
-    // };
+    document.getElementById("div1").onpaste = function(event) {
+      let items = event.clipboardData.items;
+      for (var itemIndex in items) {
+        let item = items[itemIndex];
+        if (item.kind == "file") {
+          reader.readAsDataURL(item.getAsFile());
+        }
+      }
+    };
   }
 };
 </script>
@@ -42,14 +45,18 @@ export default {
   text-align: center;
 }
 
-.main{
-    display: inline-block;
+.main {
+  display: inline-block;
 }
+
 .box {
   display: inline-block;
-  width: 200px;
-  height: 200px;
+  max-width: 200px;
+  max-height: 200px;
   margin: 10px 20px;
   background-color: rgb(255, 255, 255);
+  cursor: pointer;
 }
+
+
 </style>
