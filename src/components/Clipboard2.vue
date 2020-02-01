695e7d6f6781fd4731fed7709746ca88a4067cf2
Guilherme Brunholi <gui_brunholi@hotmail.com>
2020-01-31 22:55:44 -0300

    Teste clipboard

--- src/components/Clipboard2.vue
+++ src/components/Clipboard2.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <img id="div1" class="box" />
+      <img id="div1" class="box" @click="metodo('div1')"/>
+      <img id="div2" class="box" @click="metodo('div2')"/>
+      <img id="div3" class="box" @click="metodo('div3')"/>
     </div>
   </div>
 </template>
@@ -10,22 +12,26 @@
 export default {
   data() {
     return {
-      idDelete: ""
+      selecionado: ''
     };
   },
   methods: {
-    
+    metodo: function(id){
+      this.selecionado = id;
+      console.log(this.selecionado);
+    }
   },
   mounted() {
     var reader = new FileReader();
 
     reader.onload = function(result) {
-      let img = document.getElementById("div1");
+      let img = document.getElementById(this.selecionado);
       img.src = result.target.result;
       img.className = "box";
     };
     // Recebe o valor do clipboard
-    document.getElementById("div1").onpaste = function(event) {
+    document.getElementById(this.selecionado).onpaste = function(event) {
+      alert();
       let items = event.clipboardData.items;
       for (var itemIndex in items) {
         let item = items[itemIndex];
